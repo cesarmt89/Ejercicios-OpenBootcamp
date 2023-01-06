a6e4dae47d6b5f180aa6fcc8ce83efa54eb892b6
cesarmt89 <122001801+cesarmt89@users.noreply.github.com>
2023-01-06 04:16:24 -0300

    Condicion If

---
+++ If.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroIf = 2;
+
+        if (numeroIf > 0){
+            System.out.println("el numero es positivo");
+
+        } else if (numeroIf < 0) {
+            System.out.println("el numero es negativo");
+
+        } else
+            System.out.println("el numero es 0");
+    }
+}
