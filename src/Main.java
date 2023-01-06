26757cdf894f9a646053405c9f229620bd5d30be
cesarmt89 <122001801+cesarmt89@users.noreply.github.com>
2023-01-06 03:55:28 -0300

    agregue bucle Do While

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroDowhile = 2;
+
+        do {
+            System.out.println(numeroDowhile);
+            numeroDowhile = numeroDowhile + 1;
+        } while (numeroDowhile < 3);
+    }
+}
