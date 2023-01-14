3291790da259696dda119b1133c62719c0c1284b
Cesar Mattar <122001801+cesarmt89@users.noreply.github.com>
2023-01-14 00:19:17 -0300

    Ejercicio 1 - Java Basico

---
+++ EjercicioJavaBasico1.java
@@ -0,0 +1,32 @@
+package Ejercicio;
+
+public class EjercicioJavaBasico1 {
+    public static void main(String[] args) {
+
+        byte variable1 = 10;
+        short variable2 = 123;
+        int variable3 = 34413;
+        long variable4 = 34699331;
+        float variable5 = 12.1f;
+        double variable6 = 24.30;
+
+        char variable7 = 'a';
+
+        boolean variable8 = true;
+        boolean variable9 = false;
+
+        String variable10 = "Hola mundo!";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+        
+    }
+}
