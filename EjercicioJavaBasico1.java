7379314aad24d555f35f3cced52e5ba97a9f215a
Cesar Mattar <122001801+cesarmt89@users.noreply.github.com>
2023-01-14 00:32:56 -0300

    Update EjercicioJavaBasico1.java

--- EjercicioJavaBasico1.java
+++ EjercicioJavaBasico1.java
@@ -10,23 +10,38 @@ public class EjercicioJavaBasico1 {
         float variable5 = 12.1f;
         double variable6 = 24.30;
 
-        char variable7 = 'a';
+        char variable7 = 'a';package Ejercicio;
 
-        boolean variable8 = true;
-        boolean variable9 = false;
+public class EjercicioJavaBasico1 {
+    public static void main(String[] args) {
 
-        String variable10 = "Hola mundo!";
+        byte number1 = 10;
+        short number2 = 123;
+        int number3 = 34413;
+        long number4 = 34699331;
 
-        System.out.println(variable1);
-        System.out.println(variable2);
-        System.out.println(variable3);
-        System.out.println(variable4);
-        System.out.println(variable5);
-        System.out.println(variable6);
-        System.out.println(variable7);
-        System.out.println(variable8);
-        System.out.println(variable9);
-        System.out.println(variable10);
-        
+        System.out.println("numeros enteros: " + number1 + " " + number2 + " " + number3 + " " + number4);
+
+        float decimal1 = 12.1f;
+        double decimal2  = 24.30;
+
+        System.out.println("numeros decimales: " + decimal1 + " " + decimal2);
+
+        char caracter1 = 'a';
+
+        System.out.println("el caracter es: " + caracter1);
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println("boolean pueden ser: " + verdadero + " o " + falso);
+
+        String nombre = "Cesar";
+        String apellido = "Mattar";
+
+        System.out.println("los el nombre del autor es: " + nombre + " " + apellido);
+
+
+
     }
-}
+}       
